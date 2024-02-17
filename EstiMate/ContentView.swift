622b5d1ef802750b15956d60768cef5c2617d2d8
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2024-02-17 02:58:47 -0800

    Initial Commit

---
+++ EstiMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EstiMate
+//
+//  Created by 朱浩宇 on 2024/2/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EstiMate/EstiMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstiMateApp.swift
+//  EstiMate
+//
+//  Created by 朱浩宇 on 2024/2/17.
+//
+
+import SwiftUI
+
+@main
+struct EstiMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
